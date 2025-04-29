a196cf2c1be136916cdf3992334327d9950ecf2b
hi2242 <137996885+hi2242@users.noreply.github.com>
2025-04-29 19:20:11 +0900

    Title: 방향에 맞춰 이동, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250429/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,23 @@
+n = int(input())
+moves = [tuple(input().split()) for _ in range(n)]
+dir = [move[0] for move in moves]
+dist = [int(move[1]) for move in moves]
+
+# Please write your code here.
+
+# (0, 0)에서 시작하여 총 N번 이동
+# 첫 번째 줄에 최종 위치 x, y를 공백을 두고 출력
+
+# W(서) = (0, -1), S(남) = (1, 0), N(북) = (-1, 0), E(동) = (0, 1)
+# 1 <= N <= 100
+# 1 <= 한 번에 움직이는 거리 <= 10
+
+dx = [0, 1, -1, 0]
+dy = [-1, 0, 0, 1]
+
+dir_num = ["W", "S", "N", "E"]
+
+def solve():
+    nx = ny = 0
+    for i in range(n):
+        nx, ny = nx + (dx[moves[i][0]] * moves[i][1]), ny + (dy[moves[i][0]] * moves[i][1])
