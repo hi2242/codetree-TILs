23c8820ab51c3391fb7488f4aa9f7ac1575fd4e1
hi2242 <137996885+hi2242@users.noreply.github.com>
2025-05-01 19:30:31 +0900

    Title: 비교 연산, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250501/비교 연산/comparison-operator.py
@@ -0,0 +1,37 @@
+A, B = map(int, input().split())
+
+# A가 B보다 같거나 큰가?
+if A >= B:
+    print(1)
+else:
+    print(0)
+
+# A가 B보다 큰가?
+if A > B:
+    print(1)
+else:
+    print(0)
+
+# B가 A보다 같거나 큰가?
+if B >= A:
+    print(1)
+else:
+    print(0)
+
+# B가 A보다 큰가?
+if B > A:
+    print(1)
+else:
+    print(0)
+
+# A와 B가 같은가?
+if A == B:
+    print(1)
+else:
+    print(0)
+
+# A와 B가 다른가?
+if A != B:
+    print(1)
+else:
+    print(0)
