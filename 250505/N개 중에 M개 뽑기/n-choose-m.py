9ab9ed0abb792e8f4ead637e2d376676ce8758db
hi2242 <137996885+hi2242@users.noreply.github.com>
2025-05-05 21:23:24 +0900

    Title: N개 중에 M개 뽑기, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250505/N개 중에 M개 뽑기/n-choose-m.py
@@ -0,0 +1,27 @@
+# [0] 기본 조건
+# 숫자(1 ~ N) M개 뽑기
+
+# [1] 순서쌍 뽑기
+# combination
+def solve(a):
+    # 종료 조건
+    if a == M + 1:
+        print(*result)
+        return
+
+    # 조합
+    for i in range(1, N + 1):
+        if a <= 1 or i not in result and i > result[-1]:
+            result.append(i)
+            solve(a + 1)
+            result.pop()
+
+# 입력
+# N(숫자), M(개수)
+# 1 <= M <= N <= 10
+N, M = map(int, input().split())
+result = []
+
+# 출력
+# 조합
+solve(1)
