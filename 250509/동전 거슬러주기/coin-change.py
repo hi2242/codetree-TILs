f86701a51f73cedd872fe16a03b0f62eea0dfbc2
hi2242 <137996885+hi2242@users.noreply.github.com>
2025-05-09 21:02:04 +0900

    Title: 동전 거슬러주기, Time: 86ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250509/동전 거슬러주기/coin-change.py
@@ -0,0 +1,32 @@
+# [0] 기본 조건
+# N개의 동전
+# 금액 M을 맞추기 위한 최소 동전 수
+# 같은 동전 중복 가능
+def init():
+    for elem in coin:
+        dp[elem] = 1
+
+def solve():
+    for i in range(1, M + 1):
+        for j in coin:
+            if i - j > 0:
+                dp[i] = min(dp[i - j] + 1, dp[i])
+
+
+    return dp[M]
+
+# 입력
+# N(동전 수), M(금액)
+# 동전 종류
+# 1 <= N <= 100
+# 1 <= M <= 10000
+# 1 <= 동전의 금액 <= 10000
+N, M = map(int, input().split())
+coin = list(map(int, input().split()))
+dp = [10001 for _ in range(M + 1)]
+
+# 출력
+# 최소 동전 수 (불가능하면 -1 출력)
+init()
+
+print(solve())
