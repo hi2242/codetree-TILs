50fd0b4732f721085552005fc858c6ff45c4a244
hi2242 <137996885+hi2242@users.noreply.github.com>
2025-05-11 04:17:11 +0900

    Title: 수들의 합 최대화하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250511/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,37 @@
+# [0] 기본 정보
+# N * N 격자
+# N개의 칸 색칠
+
+# [1] 색칠하기
+# 각행, 각열에 칠해진 칸은 1칸
+# 색칠된 칸의 합 중 최댓값
+def solve(cnt, row_num):
+    global count
+    # 종료 조건
+    if cnt == N:
+        count = max(count, sum(result))
+        return
+
+    for c in list(set(std) - set(col_num)):
+        result.append(grid[row_num][c])
+        col_num.append(c)
+        solve(cnt + 1, (row_num + 1) % 3)
+        result.pop()
+        col_num.remove(c)
+
+# 입력
+# N(격자 크기)
+# 격자 정보
+# 1 <= N <= 10
+# 1 <= 주어지는 정수 <= 10000
+N = int(input())
+grid = [list(map(int, input().split())) for _ in range(N)]
+result = []
+col_num = []
+std = [i for i in range(N)]
+count = 0
+
+# 출력
+# 최댓값
+solve(0, 0)
+print(count)
