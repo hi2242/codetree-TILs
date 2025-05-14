beb29d7c38877384976a525f3779dcb00360cde7
hi2242 <137996885+hi2242@users.noreply.github.com>
2025-05-14 15:24:53 +0900

    Title: 윤년인가, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250514/윤년인가/is-leap-year.py
+++ 250514/윤년인가/is-leap-year.py
@@ -1,3 +1,3 @@
 Y = int(input())
 
-print((lambda x : "false" if x % 100 == 0 and x % 400 != 0 else "true")(Y))
+print((lambda x : "false" if x % 100 == 0 and x % 400 != 0 else "true" if x % 4 == 0 else 'false')(Y))
