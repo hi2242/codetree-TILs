8a8db9b393cdb52c8421e66f9bfeb13475c4d8a7
hi2242 <137996885+hi2242@users.noreply.github.com>
2025-05-17 16:22:56 +0900

    Title: 물의 상태, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250517/물의 상태/state-of-water.js
+++ 250517/물의 상태/state-of-water.js
@@ -5,7 +5,7 @@ let temp = Number(fs.readFileSync(0).toString().trim());
 if (temp < 0) {
     console.log("ice");
 }
-else if (temp > 100) {
+else if (temp >= 100) {
     console.log("vapor");
 }
 else {
