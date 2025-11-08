289882ee5703a16922f8b585c48b868b733ac37e
윤종근 <137996885+hi2242@users.noreply.github.com>
2025-11-08 12:39:04 +0900

    Title: 1부터의 합, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 251108/1부터의 합/sum-from-the-one.js
+++ 251108/1부터의 합/sum-from-the-one.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const input = Number(fs.readFileSync(0).toString().trim());
-let result = 0;
+let temp = 0;
+let result = null;
 for (let i = 1; i <= 100; i += 1) {
-    if (result + i >= input) {
+    if (temp + i >= input) {
+        result = i;
         break;
     }
-    result += i;
+    temp += i;
 }
 
 console.log(result);
