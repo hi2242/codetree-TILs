a4ab6e1d0f2fcd26e250743f9b183d3b21a0b8f0
윤종근 <137996885+hi2242@users.noreply.github.com>
2025-11-11 00:17:59 +0900

    Title: 두 수의 짝홀 여부, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251111/두 수의 짝홀 여부/parity-of-two-numbers.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(' ');
+const [a, b] = input.map(Number);
+
+if (a % 2 === 0) {
+    console.log('even');
+} else {
+    console.log('odd');
+}
+
+if (b % 2 === 0) {
+    console.log('even');
+} else {
+    console.log('odd');
+}
