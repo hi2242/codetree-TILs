bfcfcca3e9fb3aea4660f76149136cbc09957d46
윤종근 <137996885+hi2242@users.noreply.github.com>
2025-11-11 12:04:09 +0900

    Title: 정수의 조건 여부 3, Time: 72ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251111/정수의 조건 여부 3/numbers-condition-3.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const a = Number(input);
+
+if (a % 13 === 0 || a % 19 === 0) {
+    console.log('True');
+} else {
+    console.log('False');
+}
