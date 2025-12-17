4222882b0a3672ea73457ca2258bf13d0be03344
윤종근 <137996885+hi2242@users.noreply.github.com>
2025-12-18 01:03:04 +0900

    Title: 윤년 횟수 구하기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 251218/윤년 횟수 구하기/number-of-leap-years.js
+++ 251218/윤년 횟수 구하기/number-of-leap-years.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim();
 const N = Number(input);
 let count = 0;
 
-for (let i = 1; i < N; i += 1) {
+for (let i = 1; i <= N; i += 1) {
     if (i % 100 === 0 && i % 400 !== 0) {
         continue;
     } else if (i % 4 === 0) {
