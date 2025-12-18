b13a11ce4cf19f94fbd8a1e4f042cfda2116a718
윤종근 <137996885+hi2242@users.noreply.github.com>
2025-12-18 11:45:34 +0900

    Title: 홀수만 더하기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 251218/홀수만 더하기/add-only-odd-numbers.js
+++ 251218/홀수만 더하기/add-only-odd-numbers.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim();
-const arr = input.split('\n').map(Number);
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const N = Number(input[0])
+const arr = input.slice(1, N + 1).map(Number);
 let sumVal = 0;
 
-for (let i = 0; i < arr.length; i += 1) {
+for (let i = 0; i < N; i += 1) {
     if (arr[i] % 2 !== 0 && arr[i] % 3 === 0) {
         sumVal += arr[i];
     }
