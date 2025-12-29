dd65e321641043d04da44f11e692c3ea927c5ef5
윤종근 <137996885+hi2242@users.noreply.github.com>
2025-12-29 09:30:35 +0900

    Title: 친근하지 않은 수, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251229/친근하지 않은 수/unfriendly-number.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const N = Number(input);
+let count = 0;
+
+for (let i = 1; i <= N; i += 1) {
+    if (i % 2 === 0 || i % 3 === 0 || i % 5 === 0) {
+        continue;
+    } else {
+        count += 1;
+    }
+}
+
+console.log(count);
