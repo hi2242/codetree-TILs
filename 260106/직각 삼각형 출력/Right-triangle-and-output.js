82dfc4689e0b789204ac25e259c705a5e15510e2
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-01-06 09:25:07 +0900

    Title: 직각 삼각형 출력, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260106/직각 삼각형 출력/Right-triangle-and-output.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const N = Number(input);
+let row = null;
+
+for (let i = 0; i < N; i += 1) {
+    row = '';
+    for (let j = 0; j < 2 * i + 1; j += 1) {
+        row += '*';
+    }
+    console.log(row);
+}
