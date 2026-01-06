43055d47b9745fb5bf7c59dd71675c44ca12a7e1
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-01-06 10:04:56 +0900

    Title: 건너뛰고 별표 출력하기, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260106/건너뛰고 별표 출력하기/skip-and-print-a-star.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const N = Number(input);
+let row = null;
+
+for (let i = 1; i <= N; i += 1) {
+    row = '';
+    for (let j = 0; j < i; j += 1) {
+        row += '*';
+    }
+    console.log(`${row}\n`);
+}
+
+for (let i = N - 1; i > 0; i -= 1) {
+    row = '';
+    for (let j = 0; j < i; j += 1) {
+        row += '*';
+    }
+    console.log(`${row}\n`);
+}
