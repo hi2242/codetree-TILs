a15257898c212b12e6675d9780dbfa194c28b34e
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-01-07 09:34:13 +0900

    Title: 두 개의 직각삼각형, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260107/두 개의 직각삼각형/two-right-triangle.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const N = Number(input);
+let row = null;
+
+for (let i = N; i > 0; i -= 1) {
+    row = '';
+    for (let j = 0; j < i; j += 1) {
+        row += '*';
+    }
+
+    for (let j = 0; j < 2 * (N - i); j += 1) {
+        row += ' ';
+    }
+
+    for (let j = 0; j < i; j += 1) {
+        row += '*';
+    }
+    console.log(row);
+}
