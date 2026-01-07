26c329a355f9f6c24e5dd3200cfaa96bf089370c
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-01-07 09:21:28 +0900

    Title: 별표 출력하기 3, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260107/별표 출력하기 3/print-star-3.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const N = Number(input);
+let row = null;
+
+for (let i = 0; i < N; i += 1) {
+    row = '';
+    for (let j = 0; j < i; j += 1) {
+        row += '  ';
+    }
+    for (let j = 0; j < 2 * (N - i) - 1; j += 1) {
+        row += '* ';
+    }
+    console.log(row);
+}
