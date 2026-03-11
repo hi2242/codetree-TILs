9e9f4d75a2cdadf2ca7fa5bffbdc477d52c4b34e
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-11 17:42:03 +0900

    Title: 물의 상태, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260311/물의 상태/state-of-water.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+water_temp = int(input())
+
+if water_temp < 0:
+    print('ice')
+elif water_temp >= 100:
+    print('vapor')
+else:
+    print('water')
