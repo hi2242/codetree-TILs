b21500520093aafc4478fc919e0c422c4b328d25
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-12 15:10:12 +0900

    Title: 비교 연산, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260312/비교 연산/comparison-operator.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+
+print(1 if A >= B else 0)
+print(1 if A > B else 0)
+print(1 if B >= A else 0)
+print(1 if B > A else 0)
+print(1 if A == B else 0)
+print(1 if A != B else 0)
