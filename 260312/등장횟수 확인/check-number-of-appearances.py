425e786edab53e5eb3dc090c0cc5b142e29138db
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-12 21:07:34 +0900

    Title: 등장횟수 확인, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260312/등장횟수 확인/check-number-of-appearances.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def multi_input(n: int):
+    return [input() for _ in range(n)]
+
+# 입력부
+n_list = list(map(int, multi_input(5)))
+
+# 호출부
+count = 0
+
+for i in n_list:
+    if i % 2 == 0:
+        count += 1
+
+print(count)
