73d3b9c6d72b2f10c531e944b0396f020e80a2b4
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-12 23:20:25 +0900

    Title: 1부터 곱셈하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260312/1부터 곱셈하기/multiple-from-one.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+
+# 입력부
+N = int(input())
+
+# 호출부
+prod = 1
+for i in range(1, 10):
+    prod *= i
+    if prod >= N:
+        print(i)
+        break
