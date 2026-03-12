32e1fa2438246e607975ab2a6b08bc35088ddfaa
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-12 22:44:35 +0900

    Title: 완전수 판별기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260312/완전수 판별기/perfect-number-discriminator.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+
+# 입력부
+N = int(input())
+
+# 호출부
+acc = 0
+
+for i in range(1, N + 1):
+    if N % i == 0:
+        acc += i
+
+if acc == N:
+    print('P')
+else:
+    print('N')
