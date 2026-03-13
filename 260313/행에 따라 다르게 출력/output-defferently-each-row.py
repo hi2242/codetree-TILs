740fb4626796570612be24d9cce9d304e0b6ab4d
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-13 16:49:07 +0900

    Title: 행에 따라 다르게 출력, Time: 62ms, Memory: 18MB, Status: Passed - Codetree

--- 260313/행에 따라 다르게 출력/output-defferently-each-row.py
+++ 260313/행에 따라 다르게 출력/output-defferently-each-row.py
@@ -4,15 +4,15 @@ input = sys.stdin.readline
 
 # 선언부
 def print_number(n: int):
-    temp = 1
+    temp = 0
     for i in range(1, n + 1):
         if i % 2 != 0:
             diff = 1
         else:
             diff = 2
         for _ in range(n):
-            print(temp, end=' ')
             temp += diff
+            print(temp, end=' ')
         print()
         
 # 입력부
