87167790464931243056a8eafa9255e04f62efaa
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-13 13:24:14 +0900

    Title: 열 단위로 반복하여 출력, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260313/열 단위로 반복하여 출력/print-in-column.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def print_number(n: int):
+    for i in range(1, n + 1):
+        for _ in range(n):
+            print(i, end='')
+        print()
+        
+# 입력부
+N = int(input())
+
+# 호출부
+print_number(N)
