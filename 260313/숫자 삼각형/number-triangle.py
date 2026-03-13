a80aee36f53414a6372e3bf0aa0a0f7ee155ffb9
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-13 17:05:14 +0900

    Title: 숫자 삼각형, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260313/숫자 삼각형/number-triangle.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def print_number(n: int):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i >= j:
+                print(j, end=' ')
+            else:
+                print(' ', end=' ')
+        print()
+
+# 입력부
+N = int(input())
+
+# 호출부
+print_number(N)
