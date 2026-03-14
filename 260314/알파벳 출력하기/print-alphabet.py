8dcbb245cb557a44d42902b4c8e540493e07e019
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-14 23:21:04 +0900

    Title: 알파벳 출력하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260314/알파벳 출력하기/print-alphabet.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def print_alphabet(n: int):
+    temp = 65
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i >= j:
+                print(chr(temp), end='')
+                temp += 1
+                if temp > 90:
+                    temp = 65
+        print()
+
+# 입력부
+N = int(input())
+
+# 호출부
+print_alphabet(N)
