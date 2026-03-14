180431c06acf68b5bb3d14d04c8ac31e8980c530
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-14 22:59:45 +0900

    Title: 특이한 출력, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260314/특이한 출력/special-print.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def print_table(n: int):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            print(f'({i}, {j})', end=' ')
+            if (i + j) % 4 == 0:
+                print()
+
+# 입력부
+N = int(input())
+
+# 호출부
+print_table(N)
