2e40965c24fffc9d2b4acb2097e0d182104b2845
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-16 14:42:53 +0900

    Title: 특정 수 도달하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260316/특정 수 도달하기/reaching-specific-number.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def solve(n_list: list[int]):
+    acc = 0
+    for i in range(10):
+        if n_list[i] >= 250:
+            print(f'{acc} {acc / i:.1f}')
+            break
+        acc += n_list[i]
+
+# 구현부
+number_list = list(map(int, input().split()))
+solve(number_list)
