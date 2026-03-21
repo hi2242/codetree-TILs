00170e38b5a1488df050cff97ebe70fd321ea32e
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-21 14:45:11 +0900

    Title: 문자열 범위 출력하기 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260321/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 260321/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -7,5 +7,8 @@ input = sys.stdin.readline
 # 구현부
 s = input().rstrip()
 length = int(input())
+if length > len(s):
+    length = len(s)
+    
 for i in range(length):
     print(s[len(s) - 1 - i], end='')
