bc4ce7de56775f79f5da23b12f2cb3ac50eff147
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-21 22:43:31 +0900

    Title: 소문자와 숫자, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260321/소문자와 숫자/letter-and-number.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+
+# 구현부
+s = input().rstrip()
+for c in s:
+    if 'A' <= c <= 'Z':
+        print(c.lower(), end='')
+    elif 'a' <= c <= 'z' or '1' <= c <= '9':
+        print(c, end='')
