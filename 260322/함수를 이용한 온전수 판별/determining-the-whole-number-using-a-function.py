cc652c9a51927593accd32ab00d3e761d25412ff
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-22 17:29:07 +0900

    Title: 함수를 이용한 온전수 판별, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260322/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 260322/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -6,7 +6,7 @@ input = sys.stdin.readline
 def solve(start: int, end: int):
     count = 0
     for i in range(start, end + 1):
-        if i % 2 == 0 or i % 10 == 5 or (i % 3 == 0 and i % 9 == 0):
+        if i % 2 == 0 or i % 10 == 5 or (i % 3 == 0 and i % 9 != 0):
             continue
         else:
             count += 1
