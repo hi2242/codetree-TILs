7d3c93c0ec5549ab409a154494b8e134c903b6c0
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-22 17:52:46 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260322/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def solve(n1: int, n2: int, A: list[int], B: list[int]):
+    for i in range(n1 - n2):
+        if A[i] == B[0]:
+            for j in range(n2):
+                if A[i + j] != B[j]:
+                    break
+            else:
+                print('Yes')
+                return
+    else:
+        print('No')
+
+# 구현부
+n1, n2 = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+solve(n1, n2, A, B)
