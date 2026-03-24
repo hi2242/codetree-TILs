48193d94d556d09ee6a3c5fd4870cde8a32d49fc
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-24 11:49:46 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 75ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260324/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def solve(n: str, m: str):
+    for i in range(len(n)):
+        if n[i] == m[0]:
+            for j in range(len(m)):
+                if n[i + j] != m[j]:
+                    break
+            else:
+                print(i)
+                return
+    else:
+        print(-1)
+
+# 구현부
+N = input().rstrip()
+M = input().rstrip()
+solve(N, M)
