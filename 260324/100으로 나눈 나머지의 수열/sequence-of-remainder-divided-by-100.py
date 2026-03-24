b13397f86a8bb6695bc6974815e2419a88f9f483
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-03-24 15:42:22 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 66ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 260324/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def solve(n: int):
+    if n == 1:
+        return 2
+    elif n == 2:
+        return 4
+    return solve(n - 1) * solve(n - 2)
+
+# 구현부
+N = int(input())
+print(solve(N) % 100)
