7d1d0d7bc3950c7ef5f09cef85455dc53581d391
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-04-01 12:18:08 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260401/DateTime to DateTime/datetime-to-datetime.py
+++ 260401/DateTime to DateTime/datetime-to-datetime.py
@@ -4,10 +4,11 @@ input = sys.stdin.readline
 
 # 선언부
 def solve(A: int, B: int, C: int):
-    result = 0
-    result = (A - 11) * 24 * 60 + (B - 11) * 60 + (C - 11)
+    result = -1
+    if not (B < 11 or (B == 11 and C < 11)):
+        result = (A - 11) * 24 * 60 + (B - 11) * 60 + (C - 11)
     print(result)
-    
+
 # 구현부
 A, B, C = map(int, input().split())
 solve(A, B, C)
