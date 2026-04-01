862b655a43ea7c6881ed21b1ebf747b3156d345f
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-04-01 12:50:22 +0900

    Title: 요일 맞추기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/요일 맞추기/guess-day-of-week.py
+++ 260401/요일 맞추기/guess-day-of-week.py
@@ -5,8 +5,11 @@ input = sys.stdin.readline
 # 선언부
 def solve(m1: int, d1: int, m2: int, d2: int):
     day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    count = -1
-    for i in range(m1, m2):
+    rev = False if m1 < m2 or (m1 == m2 and d1 <= d2) else True
+    start_m, end_m = (m1, m2) if not rev else (m2, m1)
+    start_d, end_d = (d1, d2) if not rev else (d2, d1)
+    count = 0
+    for i in range(start_m, end_m):
         if i <= 7:
             if i == 2:
                 count += 28
@@ -19,7 +22,9 @@ def solve(m1: int, d1: int, m2: int, d2: int):
                 count += 31
             else:
                 count += 30
-    count = count - (d1 - 1) + d2
+    count = count - start_d + end_d
+    count = count if not rev else -count
+
     print(day_of_week[count % 7])
 
 # 구현부
