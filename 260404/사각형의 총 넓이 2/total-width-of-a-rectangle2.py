efda7843533d3a0284a10efbdd91c6a057839ee3
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-04-04 20:12:22 +0900

    Title: 사각형의 총 넓이 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260404/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 260404/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -3,23 +3,18 @@ import sys
 input = sys.stdin.readline
 
 # 선언부
-def print_extend(grid: list[int]):
-    result = 0
-    for i in range(201):
-        for j in range(201):
-            if grid[i][j] == 1:
-                result += 1
-    print(result)
-
 def solve(x1: int, y1: int, x2: int, y2: int):
+    global extend
     for r in range(y1, y2):
         for c in range(x1, x2):
-            grid[r + 100][c + 100] = 1
+            if grid[r + 100][c + 100] == 0:
+                grid[r + 100][c + 100] = 1
+                extend += 1
 # 구현부
 N = int(input())
 grid = [[0 for _ in range(201)] for _ in range(201)]
-
+extend = 0
 for _ in range(N):
     x1, y1, x2, y2 = map(int, input().split())
     solve(x1, y1, x2, y2)
-print_extend(grid)
+print(extend)
