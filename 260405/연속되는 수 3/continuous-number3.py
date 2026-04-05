4c9cfc08e899816e27774a82c5e0d6aa2635d99b
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-04-05 13:40:48 +0900

    Title: 연속되는 수 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260405/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+
+# 구현부
+N = int(input())
+max_count, count = 1, 1
+for i in range(N):
+    temp = int(input())
+    if i == 0:
+        preview = temp
+        continue
+        
+    if (temp > 0 and preview > 0) or (temp < 0 and preview < 0):
+        count += 1
+        max_count = max(max_count, count)
+        preview = temp
+
+    else:
+        max_count = max(max_count, count)
+        count = 1
+        preview = temp
+print(max_count)
