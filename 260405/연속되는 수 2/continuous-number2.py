dfc0103dc8868af2d007cc02db10ff7f5611d3e5
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-04-05 13:30:39 +0900

    Title: 연속되는 수 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260405/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+
+# 구현부
+N = int(input())
+max_count, count = 1, 1
+for i in range(N):
+    temp = int(input())
+    if i == 0:
+        preview = temp
+        continue
+    if temp == preview:
+        count += 1
+    else:
+        max_count = max(max_count, count)
+        count = 1
+        preview = temp
+print(max_count)
