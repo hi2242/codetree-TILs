5c123daac4f2b2d1ed88dbc27f46e4f04e7f9605
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-04-08 17:56:57 +0900

    Title: 벌금은 누구에게, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/벌금은 누구에게/who-will-pay.py
+++ 260408/벌금은 누구에게/who-will-pay.py
@@ -4,14 +4,21 @@ input = sys.stdin.readline
 
 # 선언부
 def solve(student_id: int):
+    result = -1
     student_list[student_id] += 1
     if student_list[student_id] == K:
-        print(student_id)
+        result = student_id
+    return result
 
 # 구현부
 N, M, K = map(int, input().split())
 student_list = [0 for _ in range(N + 1)]
+
 for _ in range(M):
     student_id = int(input())
-    solve(student_id)
-    
+    result = solve(student_id)
+    if result != -1:
+        print(result)
+        break
+else:
+    print(result)
