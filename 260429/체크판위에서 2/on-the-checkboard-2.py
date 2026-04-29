abc657fe76d2896af0fb53dfc0232db770182b21
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-04-29 22:23:11 +0900

    Title: 체크판위에서 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260429/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def solve():
+    position = [0, 0]
+    current_color = grid[0][0]
+    result = 0
+    for i in range(1, R - 1):
+        for j in range(1, C - 1):
+            if grid[i][j] != current_color:
+                for k in range(i + 1, R - 1):
+                    for l in range(j + 1, C - 1):
+                        if grid[k][l] == current_color:
+                            result += 1
+    print(result)
+
+# 구현부
+R, C = map(int, input().split())
+grid = []
+for _ in range(R):
+    grid.append(input().rstrip().split())
+solve()
