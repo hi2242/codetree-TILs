d237aa91c212c15107c503fac189975cc85e3fa6
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-04-30 10:54:45 +0900

    Title: 이상한 진수 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260430/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def translate(k: int):
+    acc = 0
+    for i in range(len(a)):
+        if i == k:
+            acc += 1 * (2 ** (len(a) - 1 - i))
+        else:
+            acc += int(a[i]) * (2 ** (len(a) - 1 - i))
+    return acc
+
+def solve():
+    result = 0
+    for i in range(len(a)):
+        result = max(result, translate(i))
+    print(result)
+
+# 구현부
+a = input().rstrip()
+solve()
