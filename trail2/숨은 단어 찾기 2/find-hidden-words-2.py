70687ad73f0d47514b18843e6c8f98a534581876
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-05-09 16:48:53 +0900

    [Passed] 숨은 단어 찾기 2 (59ms, 17MB)

--- trail2/숨은 단어 찾기 2/find-hidden-words-2.py
+++ trail2/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -3,22 +3,20 @@ import sys
 input = sys.stdin.readline
 
 # 선언부
-def print_grid(grid: list[list[int]]):
-    for row in grid:
-        print(*row)
-
-def check():
+def check(r: int, c: int, i: int) -> bool:
+    return 0 <= r + 2 * dr[i] < N and 0 <= c + 2 * dc[i] < M
 
 def solve():
+    result = 0
     for r in range(N):
         for c in range(M):
             for i in range(8):
-                if grid[r][c] == 'L' and grid[r + dr[i]][c + dc[i]]
-            if grid[r][c] == 'L':
-                check()
+                if check(r, c, i) and grid[r][c] == 'L' and grid[r + dr[i]][c + dc[i]] == 'E' and grid[r + 2 * dr[i]][c + 2 * dc[i]] == 'E':
+                    result += 1
+    print(result)
+
 # 구현부
 N, M = map(int, input().split())
 grid = [list(input().rstrip()) for _ in range(N)]
 dr, dc = [-1, -1, 0, 1, 1, 1, 0, -1], [0, 1, 1, 1, 0, -1, -1, -1]
-print_grid(grid)
 solve()
