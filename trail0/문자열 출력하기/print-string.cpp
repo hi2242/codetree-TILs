8c77370c7b7ac64efe1dae0ec6cedfd48184fdd2
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-05-16 11:40:10 +0900

    [Passed] 문자열 출력하기 (8ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s = "LeebrosCode";
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cout << s << endl;
+    return 0;
+}
