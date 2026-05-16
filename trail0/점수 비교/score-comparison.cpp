e5fd34c9ec31f9c4fb38b3259fe149fc08071058
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-05-16 13:39:57 +0900

    [Passed] 점수 비교 (8ms, 0MB)

---
+++ trail0/점수 비교/score-comparison.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A_math, A_eng, B_math, B_eng;
+    cin >> A_math >> A_eng >> B_math >> B_eng;
+    cout << (A_math > B_math && A_eng > B_eng ? 1 : 0);
+
+    return 0;
+}
