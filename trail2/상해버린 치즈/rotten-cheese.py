37b223ca8ffd551f19287fad0f8315d5faf69e7b
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-05-25 12:39:56 +0900

    [Passed] 상해버린 치즈 (56ms, 17MB)

--- trail2/상해버린 치즈/rotten-cheese.py
+++ trail2/상해버린 치즈/rotten-cheese.py
@@ -3,47 +3,42 @@ import sys
 input = sys.stdin.readline
 
 # 선언부
-def init() -> list[list[tuple[int, int]]]:
-    people = [[] for _ in range(N + 1)]
-    for p, m, t in segments_eat:
-        people[p].append((t, m))
+class Info1:
+    def __init__(self, p: int, m: int, t: int) -> None:
+        self.p, self.m, self.t = p, m, t
 
-    return people
-
-def find_hurt_cheese(people: list[list[tuple[int, int]]]) -> list[int]:
-    cheese_list = [0 for _ in range(D + 1)]
-    hurt_cheese = []
-
-    for p, st in segments_sick:
-        for pt, m in people[p]:
-            if pt < st:
-                cheese_list[m] += 1
-
-    max_count_cheese = max(cheese_list)
-    for i in range(1, D + 1):
-        if cheese_list[i] == max_count_cheese:
-            hurt_cheese.append(i)
-    
-    return hurt_cheese
-
-def find_sick_people(people: list[list[tuple[int, int]]], hurt_cheese: list[int]) -> int:
-    result = 0
-    for i in range(1, N + 1):
-        for t, m in people[i]:
-            if m in hurt_cheese:
-                result += 1
-                break
-
-    return result
+class Info2:
+    def __init__(self, p: int, t: int) -> None:
+        self.p, self.t = p, t
 
 def solve():
-    people = init()
-    hurt_cheese = find_hurt_cheese(people)
-    result = find_sick_people(people, hurt_cheese)
+    result = 0
+    for i in range(1, M + 1):
+        timeline = [0 for _ in range(N + 1)]
+        for info in info1:
+            if i != info.m:
+                continue
+            person = info.p
+            timeline[person] = info.t if timeline[person] == 0 else min(timeline[person], info.t)
+        for info in info2:
+            person = info.p
+            if timeline[person] == 0 or timeline[person] >= info.t:
+                break
+        else:
+            count = 0
+            for i in range(1, N + 1):
+                if timeline[i]:
+                    count += 1
+            result = max(result, count)
     print(result)
 
 # 구현부
 N, M, D, S = map(int, input().split())
-segments_eat = [tuple(map(int, input().split())) for _ in range(D)]
-segments_sick = [tuple(map(int, input().split())) for _ in range(S)]
+info1, info2 = [], []
+for _ in range(D):
+    p, m, t = map(int, input().split())
+    info1.append(Info1(p, m, t))
+for _ in range(S):
+    p, t = map(int, input().split())
+    info2.append(Info2(p, t))
 solve()
