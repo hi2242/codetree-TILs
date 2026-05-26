416ab32228e1665d0c32b15a07423cfbeb60c293
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-05-26 12:30:33 +0900

    [Passed] 흥미로운 숫자 2 (63ms, 17MB)

--- trail2/흥미로운 숫자 2/interesting-numbers-2.py
+++ trail2/흥미로운 숫자 2/interesting-numbers-2.py
@@ -5,16 +5,15 @@ input = sys.stdin.readline
 # 선언부
 def solve():
     result = 0
-    for i in range(X, Y + 1):
-        string_i = str(i)
-        standard = string_i[0]
-        same_count = 0
-        for j in range(1, len(string_i)):
-            # print(standard, string_i[j], standard != string_i[j])
-            if standard == string_i[j]:
-                same_count += 1
-        if len(set(string_i)) == 2 and (same_count == 0 or same_count == len(string_i) - 2):
-            result += 1
+    for i in range(len(str(X)), len(str(Y)) + 1):
+        for a in '0123456789':
+            for j in range(i):
+                for b in '0123456789':
+                    if a == b:
+                        continue
+                    temp = a * j + b + a * (i - j - 1)
+                    if int(temp[0]) != 0 and X <= int(temp) <= Y:
+                        result += 1
     print(result)
 
 # 구현부
