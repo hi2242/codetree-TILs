887da5428a1ddaaadd78e988ebedba87999a05f9
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-05-26 22:10:33 +0900

    [Passed] 팀으로 하는 틱택토 2 (50ms, 16MB)

---
+++ trail2/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+# 선언부
+def check_horizontal_vertical(result) -> None:
+    for i in range(3):
+        if len({numbers[i][j] for j in range(3)}) == 2:
+            result.add(tuple(sorted({numbers[i][j] for j in range(3)})))
+        if len({numbers[j][i] for j in range(3)}) == 2:
+            result.add(tuple(sorted({numbers[j][i] for j in range(3)})))
+
+def check_cross(result) -> None:
+    if len({numbers[i][i] for i in range(3)}) == 2:
+        result.add(tuple(sorted({numbers[i][i] for i in range(3)})))
+    if len({numbers[i][2 - i] for i in range(3)}) == 2:
+        result.add(tuple(sorted({numbers[i][i] for i in range(3)})))
+
+def solve():
+    result = set()
+    check_horizontal_vertical(result)
+    check_cross(result)
+    print(len(result))
+
+# 구현부
+numbers = [tuple(map(int, input().rstrip())) for _ in range(3)]
+solve()
