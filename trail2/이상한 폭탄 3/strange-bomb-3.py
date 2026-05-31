6ba5abf5c632f76cb389f95d0c0fe173cb1991b2
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-05-31 22:54:12 +0900

    [Passed] 이상한 폭탄 3 (47ms, 16MB)

--- trail2/이상한 폭탄 3/strange-bomb-3.py
+++ trail2/이상한 폭탄 3/strange-bomb-3.py
@@ -1,34 +1,44 @@
 import sys
 
-input = sys.stdin.readline
-
-# 선언부
-MAX_NUM = 1000000
-
-def check_between(bombs_count, exploded, start, n):
-    for i in range(n + 1):
-        if not exploded[start + i]:
-            bombs_count[bombs[start + i]] += 1
-            exploded[start + i] = 1
-
-def find_max_count(bombs_count):
-    max_count, idx = 0, 0
-    for i in range(MAX_NUM + 1):
-        if bombs_count[i] >= 2 and max_count <= bombs_count[i]:
-            max_count, idx = bombs_count[i], i
-    print(idx)
-
 def solve():
-    bombs_count = [0 for _ in range(MAX_NUM + 1)]
-    exploded = [0 for _ in range(N)]
-    for i in range(N - K):
-        for j in range(K, 0, -1):
-            if bombs[i] == bombs[i + j]:
-                check_between(bombs_count, exploded, i, j)
-                break
-    find_max_count(bombs_count)
+    input = sys.stdin.readline
+    
+    # N: 폭탄 개수, K: 제한 거리
+    N, K = map(int, input().split())
+    bombs = [int(input()) for _ in range(N)]
+    
+    # last_idx: 폭탄 번호별 최근 등장 인덱스 저장 {폭탄번호: 인덱스}
+    # exploded_counts: 폭탄 번호별 터진 횟수 저장 {폭탄번호: 카운트}
+    last_idx = {}
+    exploded_counts = {}
+    
+    for current_idx in range(N):
+        bomb = bombs[current_idx]
+        
+        # 이전에 등장한 적이 있는 폭탄인 경우
+        if bomb in last_idx:
+            # 직전 등장 위치와의 거리가 K 이하인지 확인
+            if current_idx - last_idx[bomb] <= K:
+                # 처음 터지는 폭탄이라면 자기 자신(2개)을 카운트, 이미 터진 적이 있다면 +1
+                if bomb not in exploded_counts:
+                    exploded_counts[bomb] = 2
+                else:
+                    exploded_counts[bomb] += 1
+                    
+        # 현재 인덱스를 이 폭탄의 가장 최근 위치로 갱신
+        last_idx[bomb] = current_idx
 
-# 구현부
-N, K = map(int, input().split())
-bombs = [int(input()) for _ in range(N)]
-solve()
+    # 터진 폭탄이 하나도 없는 경우 0 출력
+    if not exploded_counts:
+        print(0)
+        return
+
+    # 가장 많이 터진 횟수 찾기
+    max_count = max(exploded_counts.values())
+    
+    # 최댓값을 가진 폭탄 번호들을 모아서 그 중 가장 큰 번호 선택
+    candidates = [bomb for bomb, count in exploded_counts.items() if count == max_count]
+    print(max(candidates))
+
+if __name__ == "__main__":
+    solve()
