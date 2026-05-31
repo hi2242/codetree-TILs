24d026728126fbf35387ce4533ba2b27468f7c89
윤종근 <137996885+hi2242@users.noreply.github.com>
2026-05-31 21:36:28 +0900

    [Passed] 초기 수열 복원하기 (72ms, 18MB)

--- trail2/초기 수열 복원하기/restore-initial-sequence.py
+++ trail2/초기 수열 복원하기/restore-initial-sequence.py
@@ -4,14 +4,15 @@ input = sys.stdin.readline
 
 # 선언부
 def solve():
-    result = [0 for _ in range(N)]
     for i in range(1, N + 1):
+        result = [0 for _ in range(N)]
+        exist = [0 for _ in range(N + 1)]
         result[0] = i
         for j in range(N - 1):
-            if not (1 <= A_sums[j] - result[j] <= N) or A_sums[j] - result[j] in result:
-                result = [0 for _ in range(N)]
+            if not (1 <= A_sums[j] - result[j] <= N) or exist[A_sums[j] - result[j]]:
                 break
             result[j + 1] = A_sums[j] - result[j]
+            exist[A_sums[j] - result[j]] = 1
         else:
             print(*result)
             break
